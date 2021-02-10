a42ebf1190600266466cf23c922a6e954a0bb333
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-02-11 03:28:16 +0600

    Firebase deploy done.

---
+++ public/Config/FirebaseConfig.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBGzj0z6reD_YWNx2vb_G4o67vrfUX6cS0",
+  authDomain: "admission-consul-1576507822067.firebaseapp.com",
+  databaseURL: "https://admission-consul-1576507822067.firebaseio.com",
+  projectId: "admission-consul-1576507822067",
+  storageBucket: "admission-consul-1576507822067.appspot.com",
+  messagingSenderId: "721806796774",
+  appId: "1:721806796774:web:85ac0c242d931dabd71827",
+  measurementId: "G-912V5NTHHE",
+  crossDomain: true,
+  crossorigin: true
+};
+
+export { firebaseConfig };
